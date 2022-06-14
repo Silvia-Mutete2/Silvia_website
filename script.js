78b072a527dfde07183ebe75ef23c5636ee1b20f
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-06-14 18:17:29 +0300

    Done

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-//scrollbar
 $(document).ready(function(){
+//For the navbar to appear on scroll
+
     $(window).scroll(function(){
-        if(this.scrollY >20){
-            $('.navbar').addClass("sticky")}
-            else{
-                $('.navbar').removeClass('sticky')
-            }
+        if(this.scrollY >200){
+            $('.navbar').addClass('sticky')
+        }else{
+            $('.navbar').removeClass('sticky')
+        }
     })
+
+    var typed = new Typed(".typing-1",{
+        strings:[
+            "Fronted Developer", "Backend Developer", "Web Developer", "Freelancer"
+        ],
+        typeSpeed:80,
+        backSpeed:60,
+        loop:true
+
+    })
+
+    var typed = new Typed(".typing-2",{
+        strings:[
+            "Fronted Developer", "Backend Developer", "Web Developer", "Freelancer"
+        ],
+        typeSpeed:80,
+        backSpeed:60,
+        loop:true
+
+    })
+
+
+
 })
