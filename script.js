999583c182e17e799dbf029981d626614851de9d
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-07-01 14:40:57 +0300

    updated

--- script.js
+++ script.js
@@ -7,8 +7,20 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass('sticky')
         }
+        //Scrolling Button Btn
+        if(this.scrollY>500){
+            $('.scroll-up-btn').addClass("show")
+        }else{
+            $('.scroll-up-btn').removeClass("show")
+        }
     })
 
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0})
+    })
+
+    // typing effect
     var typed = new Typed(".typing-1",{
         strings:[
             "Fronted Developer", "Backend Developer", "Web Developer", "Freelancer"
